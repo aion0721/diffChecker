4a6c1d9eb7713dcb94d719a40bc6fac0a85c7044
aion0721 <rudolf.aion@gmail.com>
2022-04-23 17:39:26 +0900

    show tmp array

--- src/UploadForm.js
+++ src/UploadForm.js
@@ -1,13 +1,39 @@
 import react from "react";
+import { render } from "react-dom/cjs/react-dom.production.min";
 
 function UploadForm() {
+  const IRM = {
+    serv: {
+      tirmap1f_os: ["root", "login00"],
+      tirmap2f_os: ["root", "login00"],
+    },
+  };
+  var TA = {
+    serv: {},
+    ssch: {},
+    solid: {},
+    ams: {},
+  };
+  console.log(IRM);
   const getText = (e) => {
     var reader = new FileReader();
     reader.onload = function (ev) {
-      console.log(reader.result);
+      console.log(TA);
+      //console.log(e.name);
+      //console.log(reader.result);
+      const ret = reader.result.split("\r\n");
+      TA = {
+        ...TA,
+        serv: {
+          ...TA.serv,
+          [e.name]: ret,
+        },
+      };
+      console.log(TA);
     };
     reader.readAsText(e);
   };
+
   const getContents = (e) => {
     e.preventDefault();
     var file = e.target.form[0].files;
@@ -15,6 +41,7 @@ function UploadForm() {
     console.log(file.length);
     for (var i = 0; i < file.length; i++) {
       getText(file[i]);
+      document.ssch.contents.value = JSON.stringify(TA);
     }
   };
 
@@ -24,10 +51,9 @@ function UploadForm() {
   //console.log(files[0]);
   return (
     <div>
-      <form name="uploadForm">
-        <input type="file" id="serv" multiple />
+      <form name="ssch">
+        <input type="file" id="ssch" multiple />
         <br />
-        <textarea name="contents" rows="10" cols="40"></textarea>
         <button
           onClick={(e) => {
             getContents(e);
@@ -35,7 +61,18 @@ function UploadForm() {
         >
           Button
         </button>
+        <textarea name="contents" rows="10" cols="40"></textarea>
       </form>
+
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          document.ssch.contents.value = JSON.stringify(TA);
+          console.log(Object.keys(TA).map((key) => key));
+        }}
+      >
+        showTA
+      </button>
     </div>
   );
 }
